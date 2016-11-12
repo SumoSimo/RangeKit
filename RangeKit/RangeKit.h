4b9416a542479f0222235fc4411e93f0693a420b
Simon Tsai <simon.tsai.j@gmail.com>
2016-11-12 14:58:38 -0500

    Initial Commit

---
+++ RangeKit/RangeKit.h
@@ -0,0 +1,19 @@
+//
+//  RangeKit.h
+//  RangeKit
+//
+//  Created by Simon Tsai on 11/12/16.
+//  Copyright © 2016 Simon Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RangeKit.
+FOUNDATION_EXPORT double RangeKitVersionNumber;
+
+//! Project version string for RangeKit.
+FOUNDATION_EXPORT const unsigned char RangeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RangeKit/PublicHeader.h>
+
+
